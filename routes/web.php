a894caa36063a7b4af1f0db24ce13d3fa3abd3ad
Apostolis <apostolismk@gmail.com>
2020-03-12 10:56:02 +0100

    homeController name corrected

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'HomeController@index');
+Route::get('/', 'homeController@index');
 
 Route::get('movies/', 'movieController@getAllMovies');
 
