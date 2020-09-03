96bd97180053084a2c40b20e628648a3901da9cf
Apostolis Em <59648316+apostolisem@users.noreply.github.com>
2020-09-03 23:20:13 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -161,7 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Dwij\Laraadmin\LAProvider::class,
+        // Dwij\Laraadmin\LAProvider::class,
         
         /*
          * Package Service Providers...
